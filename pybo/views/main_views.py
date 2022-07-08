32425cfd9867fd488e4f2ff2f2e4d3d210aa5b67
unknown <vkclssha14@gmail.com>
2022-07-08 19:01:44 +0900

    commit

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,19 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
 
 from pybo.models import Question
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
-@bp.route('/hello')
-def hello_pybo():
-    return 'Hello, Pybo!'
-
 @bp.route('/')
 def index():
-    question_list = Question.query.order_by(Question.create_date.desc())
-    return render_template('question/question_list.html', question_list=question_list)
-
-@bp.route('/detail/<int:question_id>')
-def detail(question_id):
-    question = Question.query.get(question_id)
-    return render_template('question/question_detail.html', question=question)
+    ##블루프린트 명칭.등록된 함수명
+    ##URL매핑 규칙은 @bp.route('/list/')이므로 => /question + /list/
+    return redirect(url_for('question._list'))
